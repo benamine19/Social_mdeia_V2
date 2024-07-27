dd1905fbdbc5cc9c44b80cbc28e692fb4a10ce2d
benamine19 <129198444+benamine19@users.noreply.github.com>
2024-07-27 23:55:07 +0100

    Update Layout.js

--- src/Pages/Layout.js
+++ src/Pages/Layout.js
@@ -1,7 +1,14 @@
 import React from 'react'
-import { Outlet, Link } from "react-router-dom";
+import { Outlet, Link, Navigate } from "react-router-dom";
 import Navbare from '../components/Navbare';
+import { useSelector } from 'react-redux';
+
 function Layout() {
+    const { user } = useSelector((state) => state.user);
+    if (!user) {
+    return <Navigate to='/login' />;
+    }
+  
   return (
     <div>
     <Navbare/>
